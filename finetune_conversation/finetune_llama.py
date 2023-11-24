6fc4284d5e93db8bc0e093e54e70588ecc3475f9
Lukas Tuggener <tugg@zhaw.ch>
2023-11-24 19:35:38 +0100

    move quantization to inference

--- finetune_conversation/finetune_llama.py
+++ finetune_conversation/finetune_llama.py
@@ -189,7 +189,7 @@ def create_and_prepare_model(args):
         args.model_name,
         quantization_config=bnb_config,
         device_map=device_map,
-        use_flash_attention_2=True,
+        use_flash_attention_2=False,
         use_auth_token=True
     )
 
