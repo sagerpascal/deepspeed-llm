00f079342cd744ec3a536611b0cafbbfe350935d
Lukas Tuggener <tugg@zhaw.ch>
2023-12-12 16:57:48 +0100

    quantized inf

--- finetune_conversation/merge_finetuned.py
+++ finetune_conversation/merge_finetuned.py
@@ -9,31 +9,31 @@ import torch
 
 @dataclass
 class ScriptArguments:
-    in_adaptder: Optional[str] = field(
-        default="final_checkpoints_lora_0"
+    in_adapter: Optional[str] = field(
+        default="../final_checkpoints_lora_0"
     )
     # BNB options start here
     base_model: Optional[str] = field(
         default="meta-llama/Llama-2-7b-hf"
     )
     out_path: Optional[str] = field(
-        default=False
+        default="../merged_final_checkpoints_lora_0"
     )
 
 
 parser = HfArgumentParser(ScriptArguments)
 args = parser.parse_args_into_dataclasses()[0]
 
-model = AutoModelForCausalLM.from_pretrained(
-    args.base_model,
-    use_flash_attention_2=False,
-    use_auth_token=True
-)
+# model = AutoModelForCausalLM.from_pretrained(
+#     args.base_model,
+#     use_flash_attention_2=False,
+#     use_auth_token=True
+# )
 
 from peft import AutoPeftModelForCausalLM
+print("sadfsafdd")
 
 model = AutoPeftModelForCausalLM.from_pretrained(args.in_adapter, device_map="auto", torch_dtype=torch.bfloat16)
 model = model.merge_and_unload()
 
-output_merged_dir = "merged_"+args.in_adapter
-model.save_pretrained(output_merged_dir, safe_serialization=True)
+model.save_pretrained(args.out_path, safe_serialization=True)
