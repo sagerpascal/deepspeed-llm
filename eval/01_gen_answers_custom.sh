d5e5f17853cb05de0ae09f62dec3a9d8b22a7ba0
sagerpascal <paes.sager@gmail.com>
2023-12-14 10:21:41 +0100

    Fix path in eval scripts

--- eval/01_gen_answers_custom.sh
+++ eval/01_gen_answers_custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 
-for config in /cluster/data/tugg/lora /cluster/data/tugg/adalora /cluster/data/tugg/lokr /cluster/data/tugg/qlora /cluster/data/tugg/qadalora
+for config in /cluster/data/tugg/finalized/merged_final_checkpoints_lora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lokr_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lora_1 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_1
 do
   python eval/gen_model_answers.py --model-id $config
 done
--- eval/02_gpt4_single_judgements_custom.sh
+++ eval/02_gpt4_single_judgements_custom.sh
@@ -2,6 +2,6 @@
 echo "Bash version ${BASH_VERSION}..."
 
 cp /deepspeed-llm/data/mt_bench/model_answer/* /FastChat/fastchat/llm_judge/data/mt_bench/model_answer/
-python gen_judgment.py --model-list /cluster/data/tugg/lora /cluster/data/tugg/adalora /cluster/data/tugg/lokr /cluster/data/tugg/qlora /cluster/data/tugg/qadalora
-python show_result.py --model-list /cluster/data/tugg/lora /cluster/data/tugg/adalora /cluster/data/tugg/lokr /cluster/data/tugg/qlora /cluster/data/tugg/qadalora
+python gen_judgment.py --model-list /cluster/data/tugg/finalized/merged_final_checkpoints_lora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lokr_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lora_1 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_1
+python show_result.py --model-list /cluster/data/tugg/finalized/merged_final_checkpoints_lora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lokr_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lora_1 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_1
 
