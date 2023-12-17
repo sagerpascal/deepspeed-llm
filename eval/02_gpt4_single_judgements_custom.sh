190d4132cc3b661ef841b9fd87c108efd2634d88
sagerpascal <paes.sager@gmail.com>
2023-12-17 12:50:25 +0100

    Create eval results

--- eval/02_gpt4_single_judgements_custom.sh
+++ eval/02_gpt4_single_judgements_custom.sh
@@ -2,6 +2,6 @@
 echo "Bash version ${BASH_VERSION}..."
 
 cp /deepspeed-llm/data/mt_bench/model_answer/* /FastChat/fastchat/llm_judge/data/mt_bench/model_answer/
-python gen_judgment.py --model-list /cluster/data/tugg/finalized/merged_final_checkpoints_lora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lokr_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lora_1 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_1
-python show_result.py --model-list /cluster/data/tugg/finalized/merged_final_checkpoints_lora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lokr_0 /cluster/data/tugg/finalized/merged_final_checkpoints_lora_1 /cluster/data/tugg/finalized/merged_final_checkpoints_adalora_1
+python gen_judgment.py --model-list tugg-merged_final_checkpoints_lora_0 tugg-merged_final_checkpoints_adalora_0 tugg-merged_final_checkpoints_lokr_0 tugg-merged_final_checkpoints_lora_1 tugg-merged_final_checkpoints_adalora_1
+python show_result.py --model-list tugg-merged_final_checkpoints_lora_0 tugg-merged_final_checkpoints_adalora_0 tugg-merged_final_checkpoints_lokr_0 tugg-merged_final_checkpoints_lora_1 tugg-merged_final_checkpoints_adalora_1
 
--- eval/get_eval_stats.py
+++ eval/get_eval_stats.py
@@ -18,6 +18,11 @@ runs = {
     'vicuna-7b-v1.5-bnb-activate_4bit-deactivate_nested-float16-fp4': 'sagerpascal/mt-bench/mc421tmk',
     'vicuna-7b-v1.5-bnb-activate_4bit-activate_nested-float16-nf4': 'sagerpascal/mt-bench/6b889cu1',
     'vicuna-7b-v1.5-bnb-activate_4bit-activate_nested-float16-fp4': 'sagerpascal/mt-bench/lrfm1cp5',
+    'tugg-merged_final_checkpoints_lora_0': 'sagerpascal/mt-bench/lo350jdw',
+    'tugg-merged_final_checkpoints_adalora_0': 'sagerpascal/mt-bench/qp74qd91',
+    'tugg-merged_final_checkpoints_adalora_1': 'sagerpascal/mt-bench/34t0wrzg',
+    'tugg-merged_final_checkpoints_lokr_0': 'sagerpascal/mt-bench/5zwcs4om',
+    'tugg-merged_final_checkpoints_lora_1': 'sagerpascal/mt-bench/khnf5usr',
 }
 
 
@@ -61,25 +66,74 @@ def calculate_statistics(df: pd.DataFrame, answers, tokenizer):
         'Tokens/s': get_processed_tokens_per_s(df, answers, tokenizer)
     }
 
+
 def print_statistics(model_id, stats):
     print(f"Statistics for {model_id}:")
     for k, v in stats.items():
         print(f"\t{k: <40}: {v: <15}")
     print()
 
+
+def print_statistics(model_id, stats):
+    print(f"Statistics for {model_id}:")
+    for k, v in stats.items():
+        print(f"\t{k: <40}: {v: <15}")
+    print()
+
+def store_statistics(results):
+    df = pd.DataFrame.from_dict(results)
+    df.to_csv("results/eval_stats.csv", index=False)
+
+
+def add_statistics_to_dict(dict_, model_id, stats):
+    if 'model' not in dict_:
+        dict_['model'] = []
+    dict_['model'].append(model_id)
+    for k, v in stats.items():
+        if k not in dict_:
+            dict_[k] = []
+        dict_[k].append(v)
+    return dict_
+
+
+def run_to_model_id(run_id):
+    if run_id.startswith("tugg-"):
+        prefix = '/cluster/data/tugg/finalized'
+        return prefix + "/" + run_id[len("tugg-"):]
+    else:
+        return run_id
+
+
 def main():
-    for model_id, run_path in runs.items():
-        run, answers = import_wandb_data(run_path, model_id)
-        tokenizer = load_model(model_id, load_only_tokenizer=True)
+    results = {}
+    for run_id, run_path in runs.items():
+        run, answers = import_wandb_data(run_path, run_id)
+        tokenizer = load_model(run_to_model_id(run_id), load_only_tokenizer=True)
         system_metrics = extract_relevant_columns(run.history(stream="events"))
         stats = calculate_statistics(system_metrics, answers, tokenizer)
-        print_statistics(model_id, stats)
+        print_statistics(run_id, stats)
+        results = add_statistics_to_dict(results, run_id, stats)
+
+    store_statistics(results)
+
 
     warnings.warn(
         "This metrics are simplified. For example, we calculate tokens/s by dividing the number of tokens through "
         "the runtime and ignore the fact that during a run also the model is loaded, tokens are fed into the model"
-        " etc.")
+        " etc. However, we think that this is a good approximation, as loading the model takes about ~20s and "
+        "evaluation between 30-60min.")
 
 
 if __name__ == '__main__':
+    # run = wandb.init(project="mt-bench", group="upload-results")
+    # for fname in ["tugg-merged_final_checkpoints_adalora_0",
+    #               "tugg-merged_final_checkpoints_adalora_1",
+    #               "tugg-merged_final_checkpoints_lokr_0",
+    #               "tugg-merged_final_checkpoints_lora_0",
+    #               "tugg-merged_final_checkpoints_lora_1"]:
+    #     artifact = wandb.Artifact(name=fname + "_answers", type="jsonl")
+    #     artifact.add_file(local_path=f"/FastChat/fastchat/llm_judge/data/mt_bench/model_answer/{fname}" + ".jsonl")
+    #     run.log_artifact(artifact)
+    # run.finish()
+
     main()
